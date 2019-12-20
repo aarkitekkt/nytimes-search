54cab7d85554d10c69c77d228171aa4857e15bf7
aarkitekkt <johnson.brycej@gmail.com>
2019-12-19 21:21:06 -0700

    ajax call

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=GhMg9BIKX4bsgTVZGXqiR4ursxjB1PhI"
-var search = $("#search-term").val().trim();
-var startYear = $("#start-year").val().trim();
-var endYear = $("#end-year").val().trim();
+var nyTimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+var apiKey = "&api-key=GhMg9BIKX4bsgTVZGXqiR4ursxjB1PhI";
 var searchBtn = $("#run-search");
-
+var articleList = $("#article-section");
+var url = "";
 
 
 function searchArticles() {
+    console.log(url);
+
     $.ajax({
-        url: queryURL,
+        url: url,
         method: "GET",
-    }).then(function (reponse) {
-        console.log(reponse)
+    }).then(function (response) {
+        console.log(response)
     })
+
+}
+
+function buildQueryUrl() {
+    var search = $("#search-term").val().trim();
+    var startYear = "&begin_date=" + $("#start-year").val().trim() + "0101";
+    var endYear = "&end_date=" + $("#end-year").val().trim() + "1231";
+    url = nyTimes + "q=" + search + startYear + endYear + apiKey;
+
 }
 
 function clearFunction() {
-    $("#article-section").empty();
+    articleList.empty();
 }
 
 $("#run-search").click(function (event) {
     event.preventDefault();
+    buildQueryUrl();
     searchArticles()
 })
 
 $("#clear-all").click(function () {
     clearFunction()
 })
+
 
 
