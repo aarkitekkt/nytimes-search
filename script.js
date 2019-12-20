fe3113f35f29d100e24dfb27c513321dd41bdf36
aarkitekkt <johnson.brycej@gmail.com>
2019-12-19 20:06:13 -0700

    added search and clear function

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=GhMg9BIKX4bsgTVZGXqiR4ursxjB1PhI"
 var search = $("#search-term").val().trim();
-var startYear = $("#start-year").val.trim();
-var endYear = $("#end-year").val.trim();
+var startYear = $("#start-year").val().trim();
+var endYear = $("#end-year").val().trim();
+var searchBtn = $("#run-search");
 
-$("#run-search").click(searchBtn)
 
-function searchBtn(event) {
-    event.preventDefault();
-    console.log(search)
+
+function searchArticles() {
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (reponse) {
+        console.log(reponse)
+    })
 }
+
+function clearFunction() {
+    $("#article-section").empty();
+}
+
+$("#run-search").click(function (event) {
+    event.preventDefault();
+    searchArticles()
+})
+
+$("#clear-all").click(function () {
+    clearFunction()
+})
+
 
